23b5db0585142ed752c92c94ca306a8510f35511
Mazaiken <grigorevsega@mail.ru>
2022-11-24 16:56:04 +0300

    diceroll работает,добавление комментов

--- diceroller/src/main/java/com/example/diceroller/MainActivity.kt
+++ diceroller/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,10 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-        }
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        //Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        //Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
